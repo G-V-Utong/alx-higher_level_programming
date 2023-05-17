1e5e2cb62e91e4f2e7e97edd4992d77c7f320a0f
Godswill Utong <godswillutong@gmail.com>
2023-05-17 12:08:05 +0000

    initial commit for task 0 to 3

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,5 @@
+-- Write a script that creates the database hbtn_0c_0 in your MYSQL server.
+-- If the database already exists, your script should not fail.
+-- You are not allowed to use the SELECT or SHOW statements.
+
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
